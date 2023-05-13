3e5a19738b535bd733c33e08a44870173d326b1b
GulshannK <124583374+GulshannK@users.noreply.github.com>
2023-05-13 23:27:58 +0200

    sql for mood tracker

---
+++ moodtracker.sql
@@ -0,0 +1,14 @@
+Use mindmed;
+
+create table moodtracker (
+first_name varchar(255) NOT NULL,
+last_name varchar(255) NOT NULL,
+department varchar(255) NOT NULL,
+answer1 varchar(255) NOT NULL,
+answer2 varchar(255) NOT NULL,
+answer3 varchar(255) NOT NULL,
+answer4 varchar(255) NOT NULL,
+answer5 varchar(255) NOT NULL,
+answer6 varchar(255) NOT NULL,
+answer7 varchar(255) NOT NULL
+);
