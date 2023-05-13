706bfafeeac57386ec0dccd90fdbfc9d6324c677
JDsanto619 <123461446+JDsanto619@users.noreply.github.com>
2023-05-13 21:42:23 +0200

    tables for pomodoro

---
+++ pomodoro_createTable.sql
@@ -0,0 +1,7 @@
+create table pomodoro (
+first_name varchar(255) NOT NULL,
+last_name varchar(255) NOT NULL,
+department varchar(255) NOT NULL,
+date_of_adding_session DATE,
+num_pomodoro_session INT
+);
