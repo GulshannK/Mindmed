fd48e527c5f7c271dca228dd340a963300dd8de0
Kavish1202 <124583017+Kavish1202@users.noreply.github.com>
2023-05-14 19:37:41 +0200

    moodtracker sql

--- moodtracker.sql
+++ moodtracker.sql
@@ -1,14 +1,14 @@
 Use mindmed;
 
 create table moodtracker (
-first_name varchar(255) NOT NULL,
-last_name varchar(255) NOT NULL,
-department varchar(255) NOT NULL,
-answer1 varchar(255) NOT NULL,
-answer2 varchar(255) NOT NULL,
-answer3 varchar(255) NOT NULL,
-answer4 varchar(255) NOT NULL,
-answer5 varchar(255) NOT NULL,
-answer6 varchar(255) NOT NULL,
-answer7 varchar(255) NOT NULL
+first_name int(255) NOT NULL,
+last_name int(255) NOT NULL,
+department int(255) NOT NULL,
+answer1 int(255) NOT NULL,
+answer2 int(255) NOT NULL,
+answer3 int(255) NOT NULL,
+answer4 int(255) NOT NULL,
+answer5 int(255) NOT NULL,
+answer6 int(255) NOT NULL,
+answer7 int(255) NOT NULL
 );
