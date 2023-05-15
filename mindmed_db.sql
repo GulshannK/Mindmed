f2d366e3c50fdf3e6912f8b00a73f78fee9efb9b
Isamh11 <124450003+Isamh11@users.noreply.github.com>
2023-05-15 18:39:49 +0200

    MySql Database
    Member, Employee and Pomodoro tables

---
+++ mindmed_db.sql
@@ -0,0 +1,41 @@
+DROP TABLE member;
+drop TABLE employee;
+DROP TABLE pomodoro;
+
+CREATE TABLE member (
+  memberID INT,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  gender VARCHAR(255),
+  date_of_birth VARCHAR(255),
+  mobail_number VARCHAR(255),
+  email VARCHAR(255),
+  address VARCHAR(255),
+  city VARCHAR(255),
+  department VARCHAR(255),
+  comment VARCHAR(255)
+);
+
+
+CREATE TABLE employee (
+  employeeID INT,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  gender VARCHAR(255),
+  date_of_birth VARCHAR(255),
+  mobail_number VARCHAR(255),
+  email VARCHAR(255),
+  address VARCHAR(255),
+  city VARCHAR(255),
+  department VARCHAR(255),
+  comment VARCHAR(255)
+);
+
+
+CREATE TABLE pomodoro (
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  department VARCHAR(255),
+  num_of_session VARCHAR(255),
+  date_of_session date
+);
